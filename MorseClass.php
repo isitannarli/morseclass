e116e3872e6bcdc3e18c5ea8771f666e270891fc
isitannarli <ahmetisitannarli@gmail.com>
2016-02-19 00:47:48 +0200

    First Commit

---
+++ MorseClass.php
@@ -0,0 +1,155 @@
+<?php
+
+/**
+ * Morse Alphabet Class
+ *
+ * @author: Ahmet Işıtan Narlı
+ * @mail: ahmetisitannarli@gmail.com
+ * @git: https://github.com/isitannarli
+ * @create: 18/02/2016
+ */
+
+class MorseClass
+{
+
+    /**
+     * @var array
+     */
+    private static $alphabet = array(
+        'A' => '.-',
+        'B' => '-...',
+        'C' => '-.-.',
+        'D' => '-..',
+        'E' => '.',
+        'F' => '..-.',
+        'G' => '--.',
+        'H' => '....',
+        'I' => '..',
+        'J' => '.---',
+        'K' => '-.-',
+        'L' => '.-..',
+        'M' => '--',
+        'N' => '-.',
+        'O' => '---',
+        'P' => '.--.',
+        'Q' => '--.-',
+        'R' => '.-.',
+        'S' => '...',
+        'T' => '-',
+        'U' => '..-',
+        'V' => '...-',
+        'W' => '.--',
+        'X' => '-..-',
+        'Y' => '-.--',
+        'Z' => '--..',
+        '0' => '-----',
+        '1' => '.----',
+        '2' => '..---',
+        '3' => '...--',
+        '4' => '....-',
+        '5' => '.....',
+        '6' => '-....',
+        '7' => '--...',
+        '8' => '---..',
+        '9' => '----.',
+        '.' => '.-.-.-',
+        ',' => '--..--',
+        '?' => '..--..',
+        ':' => '---...',
+        "'" => '.----.',
+        '"' => '.-..-.',
+        '-' => '-....-',
+        '/' => '-..-.',
+        '(' => '-.--.',
+        ')' => '-.--.-',
+        'Ä' => '.-.-',
+        'Á' => '.--.-',
+        'Å' => '.--.-',
+        'Ch' => '----',
+        'É' => '..-..',
+        'Ñ' => '--.--',
+        'Ö' => '---.',
+        'Ü' => '..--',
+    );
+
+    /**
+     * @return array
+     */
+    public function alphabet()
+    {
+        $this->rev_morse = array_flip(self::$alphabet);
+        return $this->rev_morse;
+    }
+
+    /**
+     * @param $content
+     * @return string
+     */
+    public function text_to_morse($content)
+    {
+
+        $this->content = strtoupper($content);
+
+
+        $words = preg_split("/\s+/", $this->content);
+
+        $words_out = array();
+
+        foreach($words as $word)
+        {
+
+            $bits = array();
+
+            for($i=0; $i<strlen($word); $i++)
+            {
+
+                $temp = self::$alphabet[substr($word,$i,1)];
+                if ($temp) $bits[] = $temp;
+            }
+
+            $words_out[] = implode(' ', $bits);
+
+        }
+
+        return implode(' / ', $words_out);
+
+    }
+
+    /**
+     * @param $content
+     * @return mixed
+     */
+    public function morse_to_text($content)
+    {
+
+        $this->content = $content;
+
+        if(strstr($this->content, ' '))
+        {
+            $bits = explode(' ', $this->content);
+
+            foreach($bits as $bit)
+            {
+
+                if ($bit == '/')
+                {
+                    $this->out .= " ";
+                }
+                else
+                {
+                    @$this->out .= strtolower(array_search($bit, self::$alphabet));
+                }
+
+            }
+
+        }
+        else
+        {
+            @$this->out .= strtolower(array_search($this->content, self::$alphabet));
+        }
+
+        return $this->out;
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+<h1>Php Mors Alphabet Class</h1>
+
+<h2>How To Use The Class</h2>
+
+<h4>Include Class</h4>
+<code>
+require 'MorseClass.php';
+$morse = new MorseClass();
+</code>
+
+<h4>Text To Mors</h4>
+<code>
+    echo $morse->text_to_morse('hello world'); // Result: .... . .-.. .-.. --- / .-- --- .-. .-.. -..
+</code>
+
+<h4>Mors To Text</h4>
+<code>
+    echo $morse->morse_to_text('.... . .-.. .-.. --- / .-- --- .-. .-.. -..'); // Result: hello world
+</code>
+
+<h2>Author Information</h2>
+<span>Author: Ahmet Işıtan Narlı</span>
+<br>
+<span>Mail: ahmetisitannarli@gmail.com</span>
+<br>
+<span>Facebook Profile: <a href="https://facebook.com/isitan.narli">fb.com/isitan.narli</a></span>
+<br>
+<span>Twitter Profile: <a href="https://twitter.com/isitannarli">twitter.com/isitannarli</a></span>
